b03c7e0b2d471e819f92ce6653a3444aafe84f4e
Chamamme <nnyabe.chamamme@amalitech.com>
2025-08-30 08:09:39 +0000

    fix: added the htp facing routes

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,28 @@
-from django.urls import path
 from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from core.views.trip_views import TripViewSet
+from core.views.route_views import RouteSegmentViewSet
+from core.views.log_views import DailyLogViewSet, LogbookViewSet
+from core.views.duty_event_views import DutyEventViewSet
+from core.views.user_profile_views import DriverProfileViewSet, DispatcherProfileViewSet
+
+
+# Create a DRF router instance
+router = DefaultRouter()
+
+# Register your viewsets
+router.register(r"trips", TripViewSet, basename="trip")
+router.register(r"routes", RouteSegmentViewSet, basename="route")
+router.register(r"logs", DailyLogViewSet, basename="dailylog")
+router.register(r"logbooks", LogbookViewSet, basename="logbook")
+router.register(r"events", DutyEventViewSet, basename="dutyevent")
+router.register(r"drivers", DriverProfileViewSet, basename="driverprofile")
+router.register(r"dispatchers", DispatcherProfileViewSet, basename="dispatcherprofile")
+
 
 urlpatterns = [
     # Add views later
+    path("", include(router.urls)),
 ]
